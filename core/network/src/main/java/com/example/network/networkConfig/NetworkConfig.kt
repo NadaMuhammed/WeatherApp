62ee413e67f1c74742c2b3f9690b67d4415d954f
Nada Muhammed <Nada.Mohamed@mdi.com.eg>
2024-11-07 21:28:59 +0200

    Refactored network module.

--- core/network/src/main/java/com/example/network/networkConfig/NetworkConfig.kt
+++ core/network/src/main/java/com/example/network/networkConfig/NetworkConfig.kt
@@ -2,6 +2,4 @@ package com.example.network.networkConfig
 
 class NetworkConfig {
     fun getBaseUrl() = "http://api.weatherapi.com/v1"
-
-    fun getApiKey() = "5e0a87a533774dcdbb2170208240711"
 }
